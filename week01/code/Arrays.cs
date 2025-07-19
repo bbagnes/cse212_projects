9f8d2272edee0ff775aa0b7a4bcde147882e1f9d
bbagnes <bbagnes@byupathway.edu>
2025-07-19 13:49:33 -0600

    Assignment reqs.

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using Microsoft.VisualBasic;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+
 public static class Arrays
 {
     /// <summary>
@@ -6,6 +10,7 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
@@ -13,7 +18,19 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Create a list using the length value to receive each increment.
+        var list = new double[length];
+        //create a variable that will increase and be used to add each incremented value to the list.
+        double current = 0;
+        //impliment a for loop where i = 0, until i = the variable length, incremented by one (++i) to 
+        //add each incremented value of the number to the list.
+        for (int i = 0; i <= length; i++)
+        {
+            current += number;
+            list[i] = (current);
+            //Console.WriteLine(current);
+        }
+        return list; // returns generated list.
     }
 
     /// <summary>
@@ -29,5 +46,32 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Create new empty list with a length equal to the input list.
+        List<int> newList = new List<int>(data.Count);
+
+        //create a variable equal to the length of the data list.
+        int dataLength = data.Count;
+
+        //Caluculate the index value of the rotation based upon the data list length.
+        //values starting at the calcutalated value until the end of the list are the start of the new list.
+        //values starting at the beginning of the data list until the index value caluculated are the end of the new list.
+        if (amount > dataLength)
+        {
+            amount = (amount % dataLength);
+        }
+
+        //Create new list of end numbers.
+        List<int> endList = data.GetRange(0, amount);
+        //Create a new list for the starting numbers.
+        List<int> startList = data.GetRange(amount, dataLength - amount);
+        //Add the start list to the empty new List.
+        newList.AddRange(startList);
+        //Add the end list to the end of the new list.
+        newList.AddRange(endList);
+        //Print the resulting list.
+        Console.WriteLine(newList);
     }
 }
+
+
